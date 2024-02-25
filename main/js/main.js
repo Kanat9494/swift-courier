a52e175896f013bd2b97bbd90b8a8c89a0592171
Kanat9494 <kkanat94@gmail.com>
2024-02-25 23:05:52 +0600

    добавил js

---
+++ main/js/main.js
@@ -0,0 +1,12 @@
+async function sendMessageToTelegram() {
+    try {
+        const chatId = -4184487890;
+        const message = 'Привет, из моего сайта';
+        const botToken = '7096096900:AAEdfspJ0kc7n3Or893T9WJOho-iud-Ex64';
+        const url = 'https://api.telegram.org/bot${botToken}/sendMessage';
+        const response = await fetch(url, {method: 'POST'});
+    } catch (error) {
+        console.error('Ошибка при отправке сообщения в Телеграм: ', error);
+        alert('Произошла ошибка при отправке сообщения. Проверьте консоль для более подробной информации');
+    }
+}
